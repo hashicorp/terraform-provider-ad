b408e64e85de44d7b80dea17d791f047640c237c
Kyriakos Oikonomakos <kyriakos@hashicorp.com>
2020-06-03 18:01:08 +0100

    Event log section
    This commit adds support for the three GPO Security setting sections
    that deal with event logs, specifically application log, audit log, and
    system log. All three share the same fields so I'm addressing them in
    one go.

---
+++ ad/internal/gposec/audit_log.go
@@ -0,0 +1,23 @@
+package gposec
+
+import "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+
+// AuditLog represents the Audit Log section of the Security Settings GPO extension
+type AuditLog struct {
+	EventLogPolicy `ini:"Audit Log,omitempty,squash"`
+}
+
+// SetResourceData populates resource data based on the AuditLog field values
+func (p *AuditLog) SetResourceData(section string, d *schema.ResourceData) error {
+	return genericSetResourceData(section, p.EventLogPolicy, d)
+}
+
+// WriteAuditLog populates an AuditLog struct from resource data
+func WriteAuditLog(data interface{}, cfg *SecuritySettings) error {
+	elp, err := NewEventLogPolicy(data)
+	if err != nil {
+		return err
+	}
+	cfg.AuditLog = &AuditLog{EventLogPolicy: elp}
+	return nil
+}
---
+++ ad/internal/gposec/audit_log_test.go
@@ -0,0 +1,46 @@
+package gposec
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-provider-ad/ad/internal/adschema"
+)
+
+func TestWriteAuditLog(t *testing.T) {
+	data := map[string]interface{}{
+		"maximum_log_size": "10",
+	}
+
+	out := NewSecuritySettings()
+	err := WriteAuditLog(data, out)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if out.AuditLog == nil {
+		t.Errorf("AuditLog struct is nil.")
+		t.FailNow()
+	}
+
+	if out.AuditLog.MaximumLogSize != "10" {
+		t.Errorf("mismatch: MaximumLogSize. Expected 10 found %q", out.AuditLog.MaximumLogSize)
+	}
+}
+
+func TestAuditLogSetResourceData(t *testing.T) {
+	r := schema.Resource{}
+	r.Schema = adschema.GpoSecuritySchema()
+	d := r.TestResourceData()
+
+	al := AuditLog{
+		EventLogPolicy: EventLogPolicy{MaximumLogSize: "10"},
+	}
+	al.SetResourceData("audit_log", d)
+
+	mls := d.Get("audit_log.0.maximum_log_size").(string)
+
+	if mls != "10" {
+		t.Errorf("unexpected value of audit_log.0.maximum_log_size. Expected 10 got %q", mls)
+	}
+}
