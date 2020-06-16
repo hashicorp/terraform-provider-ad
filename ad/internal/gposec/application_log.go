5420724525dd8613d561ec0a467bc72967f7c0fe
Kyriakos Oikonomakos <koikonom@users.noreply.github.com>
2020-06-16 23:22:13 +0300

    GPO Security Settings resource [2/13]: Event log section (#8)
    Event log section
    
    This commit adds support for the three GPO Security setting sections
    that deal with event logs, specifically application log, audit log, and
    system log. All three share the same fields so I'm addressing them in
    one go.

---
+++ ad/internal/gposec/application_log.go
@@ -0,0 +1,23 @@
+package gposec
+
+import "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+
+// ApplicationLog represents the Application Log section of the Security Settings GPO extension
+type ApplicationLog struct {
+	EventLogPolicy `ini:"Application Log,omitempty,squash"`
+}
+
+// SetResourceData populates resource data based on the ApplicationLog field values
+func (p *ApplicationLog) SetResourceData(section string, d *schema.ResourceData) error {
+	return genericSetResourceData(section, p.EventLogPolicy, d)
+}
+
+// WriteApplicationLog populates a WriteApplicationLog struct from resource data
+func WriteApplicationLog(data interface{}, cfg *SecuritySettings) error {
+	elp, err := NewEventLogPolicy(data)
+	if err != nil {
+		return err
+	}
+	cfg.ApplicationLog = &ApplicationLog{EventLogPolicy: elp}
+	return nil
+}
