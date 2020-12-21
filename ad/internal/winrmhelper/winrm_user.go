6c4e492bb37b6e4e05372f47d09d24d8485e548b
Jeremie Patigny <jpatigny@gmail.com>
2020-12-21 12:11:46 +0100

    add-extra-attributes

--- ad/internal/winrmhelper/winrm_user.go
+++ ad/internal/winrmhelper/winrm_user.go
@@ -12,19 +12,48 @@ import (
 
 // User represents an AD User
 type User struct {
-	GUID                 string `json:"ObjectGUID"`
-	SAMAccountName       string `json:"SamAccountName"`
-	PrincipalName        string `json:"UserPrincipalName"`
-	DisplayName          string `json:"DisplayName"`
-	DistinguishedName    string `json:"DistinguishedName"`
-	UserAccountControl   int64  `json:"userAccountControl"`
-	Password             string
-	Container            string
-	Domain               string
-	Username             string
-	Enabled              bool
-	PasswordNeverExpires bool
-	CannotChangePassword bool
+	GUID                   string `json:"ObjectGUID"`
+	SAMAccountName         string `json:"SamAccountName"`
+	PrincipalName          string `json:"UserPrincipalName"`
+	City                   string
+	Company                string
+	Country                string
+	Department             string
+	Description            string
+	DisplayName            string `json:"DisplayName"`
+	DistinguishedName      string `json:"DistinguishedName"`
+	Division               string
+	EmailAddress           string
+	EmployeeID             string
+	EmployeeNumber         string
+	Enabled                bool
+	Fax                    string
+	GivenName              string
+	HomeDirectory          string
+	HomeDrive              string
+	HomePhone              string
+	HomePage               string
+	Initials               string
+	MobilePhone            string
+	Office                 string
+	OfficePhone            string
+	Organization           string
+	OtherName              string
+	POBox                  string
+	PostalCode             string
+	SmartcardLogonRequired bool
+	State                  string
+	StreetAddress          string
+	Surname                string
+	Title                  string
+	TrustedForDelegation   bool
+	UserAccountControl     int64 `json:"userAccountControl"`
+	Password               string
+	Container              string
+	Domain                 string
+	Username               string
+	PasswordNeverExpires   bool
+	CannotChangePassword   bool
 }
 
 // NewUser creates the user by running the New-ADUser powershell command
@@ -39,6 +68,8 @@ func (u *User) NewUser(client *winrm.Client) (string, error) {
 	cmds = append(cmds, fmt.Sprintf("-CannotChangePassword $%t", u.CannotChangePassword))
 	cmds = append(cmds, fmt.Sprintf("-PasswordNeverExpires $%t", u.PasswordNeverExpires))
 	cmds = append(cmds, fmt.Sprintf("-Enabled $%t", u.Enabled))
+	cmds = append(cmds, fmt.Sprintf("-SmartcardLogonRequired $%t", u.SmartcardLogonRequired))
+	cmds = append(cmds, fmt.Sprintf("-TrustedForDelegation $%t", u.TrustedForDelegation))
 
 	if u.SAMAccountName != "" {
 		cmds = append(cmds, fmt.Sprintf("-SamAccountName %q", u.SAMAccountName))
@@ -58,6 +89,114 @@ func (u *User) NewUser(client *winrm.Client) (string, error) {
 
 	if u.Container != "" {
 		cmds = append(cmds, fmt.Sprintf("-Path %q", u.Container))
+	}
+
+	if u.City != "" {
+		cmds = append(cmds, fmt.Sprintf("-City %q", u.City))
+	}
+
+	if u.Company != "" {
+		cmds = append(cmds, fmt.Sprintf("-Company %q", u.Company))
+	}
+
+	if u.Country != "" {
+		cmds = append(cmds, fmt.Sprintf("-Country %q", u.Country))
+	}
+
+	if u.Department != "" {
+		cmds = append(cmds, fmt.Sprintf("-Department %q", u.Department))
+	}
+
+	if u.Description != "" {
+		cmds = append(cmds, fmt.Sprintf("-Description %q", u.Description))
+	}
+
+	if u.Division != "" {
+		cmds = append(cmds, fmt.Sprintf("-Division %q", u.Division))
+	}
+
+	if u.EmailAddress != "" {
+		cmds = append(cmds, fmt.Sprintf("-EmailAddress %q", u.EmailAddress))
+	}
+
+	if u.EmployeeID != "" {
+		cmds = append(cmds, fmt.Sprintf("-EmployeeID %q", u.EmployeeID))
+	}
+
+	if u.EmployeeNumber != "" {
+		cmds = append(cmds, fmt.Sprintf("-EmployeeNumber %q", u.EmployeeNumber))
+	}
+
+	if u.Fax != "" {
+		cmds = append(cmds, fmt.Sprintf("-Fax %q", u.Fax))
+	}
+
+	if u.GivenName != "" {
+		cmds = append(cmds, fmt.Sprintf("-GivenName %q", u.GivenName))
+	}
+
+	if u.HomeDirectory != "" {
+		cmds = append(cmds, fmt.Sprintf("-HomeDirectory %q", u.HomeDirectory))
+	}
+
+	if u.HomeDrive != "" {
+		cmds = append(cmds, fmt.Sprintf("-HomeDrive %q", u.HomeDrive))
+	}
+
+	if u.HomePhone != "" {
+		cmds = append(cmds, fmt.Sprintf("-HomePhone %q", u.HomePhone))
+	}
+
+	if u.HomePage != "" {
+		cmds = append(cmds, fmt.Sprintf("-HomePage %q", u.HomePage))
+	}
+
+	if u.Initials != "" {
+		cmds = append(cmds, fmt.Sprintf("-Initials %q", u.Initials))
+	}
+
+	if u.MobilePhone != "" {
+		cmds = append(cmds, fmt.Sprintf("-MobilePhone %q", u.MobilePhone))
+	}
+
+	if u.Office != "" {
+		cmds = append(cmds, fmt.Sprintf("-Office %q", u.Office))
+	}
+
+	if u.OfficePhone != "" {
+		cmds = append(cmds, fmt.Sprintf("-OfficePhone %q", u.OfficePhone))
+	}
+
+	if u.Organization != "" {
+		cmds = append(cmds, fmt.Sprintf("-Organization %q", u.Organization))
+	}
+
+	if u.OtherName != "" {
+		cmds = append(cmds, fmt.Sprintf("-OtherName %q", u.OtherName))
+	}
+
+	if u.POBox != "" {
+		cmds = append(cmds, fmt.Sprintf("-POBox %q", u.POBox))
+	}
+
+	if u.PostalCode != "" {
+		cmds = append(cmds, fmt.Sprintf("-PostalCode %q", u.PostalCode))
+	}
+
+	if u.State != "" {
+		cmds = append(cmds, fmt.Sprintf("-State %q", u.State))
+	}
+
+	if u.StreetAddress != "" {
+		cmds = append(cmds, fmt.Sprintf("-StreetAddress %q", u.StreetAddress))
+	}
+
+	if u.Surname != "" {
+		cmds = append(cmds, fmt.Sprintf("-Surname %q", u.Surname))
+	}
+
+	if u.Title != "" {
+		cmds = append(cmds, fmt.Sprintf("-Title %q", u.Title))
 	}
 
 	result, err := RunWinRMCommand(client, cmds, true, false)
@@ -88,6 +227,33 @@ func (u *User) ModifyUser(d *schema.ResourceData, client *winrm.Client) error {
 		"sam_account_name": "SamAccountName",
 		"display_name":     "DisplayName",
 		"principal_name":   "UserPrincipalName",
+		"city":             "City",
+		"company":          "Company",
+		"country":          "Country",
+		"department":       "Department",
+		"description":      "Description",
+		"division":         "Division",
+		"email_address":    "EmailAddress",
+		"employee_id":      "EmployeeID",
+		"employee_number":  "EmployeeNumber",
+		"fax":              "Fax",
+		"given_name":       "GivenName",
+		"home_directory":   "HomeDirectory",
+		"home_drive":       "HomeDrive",
+		"home_phone":       "HomePhone",
+		"home_page":        "HomePage",
+		"initials":         "Initials",
+		"mobile_phone":     "MobilePhone",
+		"office":           "Office",
+		"office_phone":     "OfficePhone",
+		"organization":     "Organization",
+		"other_name":       "OtherName",
+		"po_box":           "POBox",
+		"postal_code":      "PostalCode",
+		"state":            "State",
+		"street_address":   "StreetAddress",
+		"surname":          "Surname",
+		"title":            "Title",
 	}
 
 	cmds := []string{fmt.Sprintf("Set-ADUser -Identity %q", u.GUID)}
@@ -100,9 +266,11 @@ func (u *User) ModifyUser(d *schema.ResourceData, client *winrm.Client) error {
 	}
 
 	boolKeyMap := map[string]string{
-		"cannot_change_password": "CannotChangePassword",
-		"password_never_expires": "PasswordNeverExpires",
-		"enabled":                "Enabled",
+		"cannot_change_password":    "CannotChangePassword",
+		"password_never_expires":    "PasswordNeverExpires",
+		"enabled":                   "Enabled",
+		"smart_card_logon_required": "SmartcardLogonRequired",
+		"trusted_for_delegation":    "TrustedForDelegation",
 	}
 
 	for k, param := range boolKeyMap {
@@ -167,15 +335,44 @@ func (u *User) DeleteUser(client *winrm.Client) error {
 // GetUserFromResource returns a user struct built from Resource data
 func GetUserFromResource(d *schema.ResourceData) *User {
 	user := User{
-		GUID:                 d.Id(),
-		SAMAccountName:       SanitiseTFInput(d, "sam_account_name"),
-		PrincipalName:        SanitiseTFInput(d, "principal_name"),
-		DisplayName:          SanitiseTFInput(d, "display_name"),
-		Container:            SanitiseTFInput(d, "container"),
-		Password:             SanitiseTFInput(d, "initial_password"),
-		Enabled:              d.Get("enabled").(bool),
-		PasswordNeverExpires: d.Get("password_never_expires").(bool),
-		CannotChangePassword: d.Get("cannot_change_password").(bool),
+		GUID:                   d.Id(),
+		SAMAccountName:         SanitiseTFInput(d, "sam_account_name"),
+		PrincipalName:          SanitiseTFInput(d, "principal_name"),
+		DisplayName:            SanitiseTFInput(d, "display_name"),
+		Container:              SanitiseTFInput(d, "container"),
+		Password:               SanitiseTFInput(d, "initial_password"),
+		Enabled:                d.Get("enabled").(bool),
+		PasswordNeverExpires:   d.Get("password_never_expires").(bool),
+		CannotChangePassword:   d.Get("cannot_change_password").(bool),
+		City:                   SanitiseTFInput(d, "city"),
+		Company:                SanitiseTFInput(d, "company"),
+		Country:                SanitiseTFInput(d, "country"),
+		Department:             SanitiseTFInput(d, "department"),
+		Description:            SanitiseTFInput(d, "description"),
+		Division:               SanitiseTFInput(d, "division"),
+		EmailAddress:           SanitiseTFInput(d, "email_address"),
+		EmployeeID:             SanitiseTFInput(d, "employee_id"),
+		EmployeeNumber:         SanitiseTFInput(d, "employee_number"),
+		Fax:                    SanitiseTFInput(d, "fax"),
+		GivenName:              SanitiseTFInput(d, "given_name"),
+		HomeDirectory:          SanitiseTFInput(d, "home_directory"),
+		HomeDrive:              SanitiseTFInput(d, "home_drive"),
+		HomePhone:              SanitiseTFInput(d, "home_phone"),
+		HomePage:               SanitiseTFInput(d, "home_page"),
+		Initials:               SanitiseTFInput(d, "initials"),
+		MobilePhone:            SanitiseTFInput(d, "mobile_phone"),
+		Office:                 SanitiseTFInput(d, "office"),
+		OfficePhone:            SanitiseTFInput(d, "office_phone"),
+		Organization:           SanitiseTFInput(d, "organization"),
+		OtherName:              SanitiseTFInput(d, "other_name"),
+		POBox:                  SanitiseTFInput(d, "po_box"),
+		PostalCode:             SanitiseTFInput(d, "postal_code"),
+		SmartcardLogonRequired: d.Get("smart_card_logon_required").(bool),
+		State:                  SanitiseTFInput(d, "state"),
+		StreetAddress:          SanitiseTFInput(d, "street_address"),
+		Surname:                SanitiseTFInput(d, "surname"),
+		Title:                  SanitiseTFInput(d, "title"),
+		TrustedForDelegation:   d.Get("trusted_for_delegation").(bool),
 	}
 	if user.PrincipalName != "" {
 		tokens := strings.Split(user.PrincipalName, "@")
--- ad/resource_ad_user.go
+++ ad/resource_ad_user.go
@@ -64,6 +64,153 @@ func resourceADUser() *schema.Resource {
 				Default:     true,
 				Description: "If set to false, the user will be disabled.",
 			},
+			"city": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's town or city. This parameter sets the City property of a user object.",
+			},
+			"company": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's company. This parameter sets the Company property of a user object.",
+			},
+			"country": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the country or region code for the user's language of choice. This parameter sets the Country property of a user object.",
+			},
+			"department": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's department. This parameter sets the Department property of a user object.",
+			},
+			"description": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies a description of the object. This parameter sets the value of the Description property for the user object.",
+			},
+			"division": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's division. This parameter sets the Division property of a user object.",
+			},
+			"email_address": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's e-mail address. This parameter sets the EmailAddress property of a user object.",
+			},
+			"employee_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's employee ID. This parameter sets the EmployeeID property of a user object.",
+			},
+			"employee_number": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's employee number. This parameter sets the EmployeeNumber property of a user object.",
+			},
+			"fax": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's fax phone number. This parameter sets the Fax property of a user object.",
+			},
+			"given_name": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's given name. This parameter sets the GivenName property of a user object.",
+			},
+			"home_directory": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies a user's home directory. This parameter sets the HomeDirectory property of a user object.",
+			},
+			"home_drive": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies a drive that is associated with the UNC path defined by the HomeDirectory property. The drive letter is specified as <DriveLetter>: where <DriveLetter> indicates the letter of the drive to associate. The <DriveLetter> must be a single, uppercase letter and the colon is required. This parameter sets the HomeDrive property of the user object.",
+			},
+			"home_phone": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's home telephone number. This parameter sets the HomePhone property of a user object.",
+			},
+			"home_page": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the URL of the home page of the object. This parameter sets the homePage property of a user object.",
+			},
+			"initials": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the initials that represent part of a user's name. You can use this value for the user's middle initial. This parameter sets the Initials property of a user object.",
+			},
+			"mobile_phone": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's mobile phone number. This parameter sets the MobilePhone property of a user object.",
+			},
+			"office": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the location of the user's office or place of business. This parameter sets the Office property of a user object.",
+			},
+			"office_phone": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's office telephone number. This parameter sets the OfficePhone property of a user object.",
+			},
+			"organization": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's organization. This parameter sets the Organization property of a user object.",
+			},
+			"other_name": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies a name in addition to a user's given name and surname, such as the user's middle name.",
+			},
+			"po_box": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's post office box number. This parameter sets the POBox property of a user object.",
+			},
+			"postal_code": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's postal code or zip code. This parameter sets the PostalCode property of a user object.",
+			},
+			"smart_card_logon_required": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "If set to true, a smart card is required to logon. This parameter sets the SmartCardLoginRequired property for a user object.",
+			},
+			"state": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's or Organizational Unit's state or province. This parameter sets the State property of a user object.",
+			},
+			"street_address": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's street address. This parameter sets the StreetAddress property of a user object.",
+			},
+			"surname": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's last name or surname. This parameter sets the Surname property of a user object.",
+			},
+			"title": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Specifies the user's title. This parameter sets the Title property of a user object",
+			},
+			"trusted_for_delegation": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "If set to true, the user account is trusted for Kerberos delegation. A service that runs under an account that is trusted for Kerberos delegation can assume the identity of a client requesting the service. This parameter sets the TrustedForDelegation property of an account object.",
+			},
 		},
 	}
 }
@@ -111,6 +258,33 @@ func resourceADUserRead(d *schema.ResourceData, meta interface{}) error {
 	_ = d.Set("enabled", u.Enabled)
 	_ = d.Set("password_never_expires", u.PasswordNeverExpires)
 	_ = d.Set("cannot_change_password", u.CannotChangePassword)
+	_ = d.Set("city", u.City)
+	_ = d.Set("company", u.Company)
+	_ = d.Set("country", u.Country)
+	_ = d.Set("department", u.Department)
+	_ = d.Set("description", u.Description)
+	_ = d.Set("division", u.Division)
+	_ = d.Set("email_address", u.EmailAddress)
+	_ = d.Set("employee_id", u.EmployeeID)
+	_ = d.Set("employee_number", u.EmployeeNumber)
+	_ = d.Set("fax", u.Fax)
+	_ = d.Set("given_name", u.GivenName)
+	_ = d.Set("home_directory", u.HomeDirectory)
+	_ = d.Set("home_drive", u.HomeDrive)
+	_ = d.Set("home_phone", u.HomePhone)
+	_ = d.Set("home_page", u.HomePage)
+	_ = d.Set("initials", u.Initials)
+	_ = d.Set("mobile_phone", u.MobilePhone)
+	_ = d.Set("office", u.Office)
+	_ = d.Set("office_phone", u.OfficePhone)
+	_ = d.Set("organization", u.Organization)
+	_ = d.Set("other_name", u.OtherName)
+	_ = d.Set("po_box", u.POBox)
+	_ = d.Set("postal_code", u.PostalCode)
+	_ = d.Set("state", u.State)
+	_ = d.Set("street_address", u.StreetAddress)
+	_ = d.Set("surname", u.Surname)
+	_ = d.Set("title", u.Title)
 
 	return nil
 }
