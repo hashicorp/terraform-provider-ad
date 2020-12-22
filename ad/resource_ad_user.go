c6d5f37f426b230e09f2aa2504988602f3c40dda
Jeremie Patigny <jpatigny@gmail.com>
2020-12-22 11:25:50 +0100

    using-validation-from-sdk add-input-validation-for-country add-input-validation-for-initials

--- ad/resource_ad_user.go
+++ ad/resource_ad_user.go
@@ -3,11 +3,15 @@ package ad
 import (
 	"fmt"
 	"log"
+	"regexp"
 	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-ad/ad/internal/winrmhelper"
 )
+
+const countryRegexp = `^(A(D|E|F|G|I|L|M|N|O|R|S|T|Q|U|W|X|Z)|B(A|B|D|E|F|G|H|I|J|L|M|N|O|R|S|T|V|W|Y|Z)|C(A|C|D|F|G|H|I|K|L|M|N|O|R|U|V|X|Y|Z)|D(E|J|K|M|O|Z)|E(C|E|G|H|R|S|T)|F(I|J|K|M|O|R)|G(A|B|D|E|F|G|H|I|L|M|N|P|Q|R|S|T|U|W|Y)|H(K|M|N|R|T|U)|I(D|E|Q|L|M|N|O|R|S|T)|J(E|M|O|P)|K(E|G|H|I|M|N|P|R|W|Y|Z)|L(A|B|C|I|K|R|S|T|U|V|Y)|M(A|C|D|E|F|G|H|K|L|M|N|O|Q|P|R|S|T|U|V|W|X|Y|Z)|N(A|C|E|F|G|I|L|O|P|R|U|Z)|OM|P(A|E|F|G|H|K|L|M|N|R|S|T|W|Y)|QA|R(E|O|S|U|W)|S(A|B|C|D|E|G|H|I|J|K|L|M|N|O|R|T|V|Y|Z)|T(C|D|F|G|H|J|K|L|M|N|O|R|T|V|W|Z)|U(A|G|M|S|Y|Z)|V(A|C|E|G|I|N|U)|W(F|S)|Y(E|T)|Z(A|M|W))$`
 
 func resourceADUser() *schema.Resource {
 	return &schema.Resource{
@@ -75,9 +79,10 @@ func resourceADUser() *schema.Resource {
 				Description: "Specifies the user's company. This parameter sets the Company property of a user object.",
 			},
 			"country": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "Specifies the country or region code for the user's language of choice. This parameter sets the Country property of a user object.",
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringMatch(regexp.MustCompile(countryRegexp), "Must be a valid two characters country code."),
+				Description:  "Specifies the country by setting the country code (refer to ISO 3166)",
 			},
 			"department": {
 				Type:        schema.TypeString,
@@ -140,9 +145,10 @@ func resourceADUser() *schema.Resource {
 				Description: "Specifies the URL of the home page of the object. This parameter sets the homePage property of a user object.",
 			},
 			"initials": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "Specifies the initials that represent part of a user's name. You can use this value for the user's middle initial. This parameter sets the Initials property of a user object.",
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringLenBetween(0, 6),
+				Description:  "Specifies the initials that represent part of a user's name. Maximum 6 char.",
 			},
 			"mobile_phone": {
 				Type:        schema.TypeString,
