48b403f97879dfd2aac1daa28edddaf7736683bc
Jeremie Patigny <jpatigny@gmail.com>
2021-01-06 17:27:02 +0100

    country-validation-less-restrictive

--- ad/resource_ad_user.go
+++ ad/resource_ad_user.go
@@ -11,8 +11,6 @@ import (
 	"github.com/hashicorp/terraform-provider-ad/ad/internal/winrmhelper"
 )
 
-const countryRegexp = `^(A(D|E|F|G|I|L|M|N|O|R|S|T|Q|U|W|X|Z)|B(A|B|D|E|F|G|H|I|J|L|M|N|O|R|S|T|V|W|Y|Z)|C(A|C|D|F|G|H|I|K|L|M|N|O|R|U|V|X|Y|Z)|D(E|J|K|M|O|Z)|E(C|E|G|H|R|S|T)|F(I|J|K|M|O|R)|G(A|B|D|E|F|G|H|I|L|M|N|P|Q|R|S|T|U|W|Y)|H(K|M|N|R|T|U)|I(D|E|Q|L|M|N|O|R|S|T)|J(E|M|O|P)|K(E|G|H|I|M|N|P|R|W|Y|Z)|L(A|B|C|I|K|R|S|T|U|V|Y)|M(A|C|D|E|F|G|H|K|L|M|N|O|Q|P|R|S|T|U|V|W|X|Y|Z)|N(A|C|E|F|G|I|L|O|P|R|U|Z)|OM|P(A|E|F|G|H|K|L|M|N|R|S|T|W|Y)|QA|R(E|O|S|U|W)|S(A|B|C|D|E|G|H|I|J|K|L|M|N|O|R|T|V|Y|Z)|T(C|D|F|G|H|J|K|L|M|N|O|R|T|V|W|Z)|U(A|G|M|S|Y|Z)|V(A|C|E|G|I|N|U)|W(F|S)|Y(E|T)|Z(A|M|W))$`
-
 func resourceADUser() *schema.Resource {
 	return &schema.Resource{
 		Description: "`ad_user` manages User objects in an Active Directory tree.",
@@ -81,7 +79,7 @@ func resourceADUser() *schema.Resource {
 			"country": {
 				Type:         schema.TypeString,
 				Optional:     true,
-				ValidateFunc: validation.StringMatch(regexp.MustCompile(countryRegexp), "Must be a valid two characters country code."),
+				ValidateFunc: validation.StringLenBetween(2, 3),
 				Description:  "Specifies the country by setting the country code (refer to ISO 3166)",
 			},
 			"department": {
