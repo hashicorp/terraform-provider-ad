b99d1a830137c98e2c256d3c0d0593fbbe48a889
Jeremie Patigny <jpatigny@gmail.com>
2021-01-31 11:16:11 +0100

    fix-examples-for-provider-including-local

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,21 +1,52 @@
-resource "ad_ou" "o" { 
-    name = "gplinktestOU"
-    path = "dc=yourdomain,dc=com"
-    description = "OU for gplink tests"
-    protected = false
-}
-    
-resource "ad_gpo" "g" {
-    name        = "gplinktestGPO"
-    domain      = "yourdomain.com"
-    description = "gpo for gplink tests"
-    status      = "AllSettingsEnabled"
+variable "hostname" { default = "ad.yourdomain.com" }
+variable "username" { default = "user" }
+variable "password" { default = "password" }
+
+// remote using Basic authentication
+provider "ad" {
+  winrm_hostname = var.hostname
+  winrm_username = var.username
+  winrm_password = var.password
 }
 
-resource "ad_gplink" "og" { 
-    gpo_guid = ad_gpo.g.id
-    target_dn = ad_ou.o.dn
-    enforced = true
-    enabled = true
-    order = 0
+// remote using NTLM authentication
+provider "ad" {
+  winrm_hostname = var.hostname
+  winrm_username = var.username
+  winrm_password = var.password
+  winrm_use_ntlm = true
+}
+
+// remote using NTLM authentication and HTTPS 
+provider "ad" {
+  winrm_hostname = var.hostname
+  winrm_username = var.username
+  winrm_password = var.password
+  winrm_use_ntlm = true
+  winrm_port     = 5986
+  winrm_proto    = "https"
+  winrm_insecure = true
+}
+
+// remote using Kerberos authentication  
+provider "ad" {
+  winrm_hostname = var.hostname
+  winrm_username = var.username
+  winrm_password = var.password
+  krb_realm      = "YOURDOMAIN.COM"
+}
+
+// remote using Kerberos authentication with krb5.conf file
+provider "ad" {
+  winrm_hostname = var.hostname
+  winrm_username = var.username
+  winrm_password = var.password
+  krb_conf       = "/etc/krb5.conf"
+}
+
+// local (windows only)
+provider "ad" {
+  winrm_hostname = ""
+  winrm_username = ""
+  winrm_password = ""
 }
